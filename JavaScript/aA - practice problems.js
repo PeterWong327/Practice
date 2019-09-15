5117eb73e6a681395a50d548926e4597cd4d3d3c
“Peter <peterwongisnow@gmail.com>
2019-09-14 23:55:37 -0700

    Add madLib problem

--- JavaScript/aA - practice problems.js
+++ JavaScript/aA - practice problems.js
@@ -58,3 +58,18 @@ function mysteryScoping5() {
 }
 // prints out:
 // SyntaxError: Identifier 'x' has already been declared
+
+
+// madLib
+// Write a function that takes three strings - a verb, an adjective, and
+// a noun - uppercases and interpolates them into the sentence
+//"We shall VERB the ADJECTIVE NOUN". Use ES6 template literals.
+//
+// For example,
+//
+// > madLib('make', 'best', 'guac');
+// "We shall MAKE the BEST GUAC."
+
+function madLib(verb,adjective,noun) {
+  console.log('We shall ${verb} the ${adjective} ${noun}.');
+}
